c66110d19730fa14e871f1e989b039a965d8d69f
chtnnnmtgkyp <cauhoctrongocnghech_mdhp@yahoo.com>
2016-04-13 17:02:25 +0700

    Added files via upload

---
+++ Assignment 5/Slider_Control /ADVCustomUISlider/ADVCustomUISlider/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ADVCustomUISlider
+//
+//  Created by Tope on 13/02/2013.
+//  Copyright (c) 2013 App Design Vault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Assignment 5/Slider_Control /ADVCustomUISlider/ADVCustomUISlider/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ADVCustomUISlider
+//
+//  Created by Tope on 13/02/2013.
+//  Copyright (c) 2013 App Design Vault. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
